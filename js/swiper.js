40dcd5f3905ede7384e4bd612dfcb338a6e94d41
agreytosira <agreytosira@gmail.com>
2020-10-05 16:02:43 +0700

    Update 5 Okt 2020

---
+++ js/swiper.js
@@ -0,0 +1,25 @@
+// SwiperJS
+const mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    autoplay: {
+        delay: 2500,
+        loop: true,
+        disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
