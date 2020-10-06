78ddcbdb9d0de90a5ac42a9b30af32799538343d
agreytosira <agreytosira@gmail.com>
2020-10-06 11:59:36 +0700

    Optimasi Size Gambar

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,18 @@
 // SwiperJS
 const mySwiper = new Swiper('.swiper-container', {
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
     // Optional parameters
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
     autoplay: {
         delay: 2500,
         loop: true,
